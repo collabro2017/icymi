06e8ab0bd8f609910156f826506040160fbced89
lion <lion@lions-Mac-6.local>
2016-11-06 19:31:00 -0800

    added crop and draw text

---
+++ OmnEvent/Classes/OMDrawTextViewController.h
@@ -0,0 +1,13 @@
+//
+//  OMDrawTextViewController.h
+//  ICYMI
+//
+//  Created by lion on 11/6/16.
+//  Copyright © 2016 ellisa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OMDrawTextViewController : UIViewController
+
+@end
