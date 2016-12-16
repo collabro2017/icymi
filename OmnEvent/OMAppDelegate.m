1ce1cb3922fd15f530e7cab4e4492b1161746390
Macx <Macx@JNC-MAC.local>
2016-12-16 03:46:09 +0100

    fix bug

--- OmnEvent/OMAppDelegate.m
+++ OmnEvent/OMAppDelegate.m
@@ -55,13 +55,15 @@
     [OMSocialEvent registerSubclass];
     [ParseCrashReporting enable];
     [Parse enableLocalDatastore];
-    [Parse setApplicationId:PARSE_APP_ID clientKey:CLIENT_KEY];
-//    [Parse initializeWithConfiguration:[ParseClientConfiguration configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
-//        configuration.applicationId = PARSE_APP_ID;
-//        configuration.clientKey = CLIENT_KEY;
-//        configuration.server = @"http://eb-icymyi-parse-server.jegr4ium5p.us-east-1.elasticbeanstalk.com/parse";
-//    }]];
-//    
+    //[Parse setApplicationId:PARSE_APP_ID clientKey:CLIENT_KEY];
+    
+    [Parse initializeWithConfiguration:[ParseClientConfiguration configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+        configuration.applicationId = PARSE_APP_ID;
+        configuration.clientKey = CLIENT_KEY;
+        configuration.server = @"http://eb-icymyi-parse-server.jegr4ium5p.us-east-1.elasticbeanstalk.com/parse";
+        [configuration setLocalDatastoreEnabled:YES ];
+    }]];
+    
     if (IS_UPLOADING)
         [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     
